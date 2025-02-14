59c38000ceef283a035f97e0e9e60085c57d634c
OmBiradar <ombiradar04@gmail.com>
2025-02-14 20:07:36 +0530

    minor update

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -10,19 +10,19 @@ export default function Footer() {
                     </div>
                     <div className="flex space-x-4">
                         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                            <Image src="/facebook-icon.png" alt="Facebook" width={24} height={24} className="h-6 w-6" />
+                            <Image src="./facebook-icon.png" alt="Facebook" width={24} height={24} className="h-6 w-6" />
                         </a>
                         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                            <Image src="/twitter-icon.png" alt="Twitter" width={24} height={24} className="h-6 w-6" />
+                            <Image src="./twitter-icon.png" alt="Twitter" width={24} height={24} className="h-6 w-6" />
                         </a>
                         <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                            <Image src="/instagram-icon.png" alt="Instagram" width={24} height={24} className="h-6 w-6" />
+                            <Image src="./instagram-icon.png" alt="Instagram" width={24} height={24} className="h-6 w-6" />
                         </a>
                         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                            <Image src="/linkedin-icon.png" alt="LinkedIn" width={24} height={24} className="h-6 w-6" />
+                            <Image src="./linkedin-icon.png" alt="LinkedIn" width={24} height={24} className="h-6 w-6" />
                         </a>
                         <a href="mailto:contact@rocketteam.com" target="_blank" rel="noopener noreferrer">
-                            <Image src="/email-icon.png" alt="Email" width={24} height={24} className="h-6 w-6" />
+                            <Image src="./email-icon.png" alt="Email" width={24} height={24} className="h-6 w-6" />
                         </a>
                     </div>
                 </div>
