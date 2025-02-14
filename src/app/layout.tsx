c612ba049721a4aa24f4ef3f43d7c2e5b3a16d76
OmBiradar <ombiradar04@gmail.com>
2025-02-14 21:08:25 +0530

    fixed title and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image'
 import Navbar from "./components/navbar"
 import Footer from "./components/footer"
 import './globals.css'
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Next.js App",
+  icons: {
+    icon: "/rocket-icon.png",
+    shortcut: "/rocket-icon.png",
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Rocket Team</title>
+        <link rel="icon" type="image/png" href="./rocket-icon.png"></link>
+      </head>
       <body>
         <Navbar />
         <main className="container mx-auto py-10">{children}</main>
