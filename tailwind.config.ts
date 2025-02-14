58847d5d42e99181a585b7fa8da1fc9ca3d553ac
OmBiradar <ombiradar04@gmail.com>
2025-02-15 02:12:30 +0530

    minor fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,13 +70,13 @@ export default {
 		},
 		keyframes: {
 			fadeIn: {
-				from: { opacity: 0, transform: 'translateY(30px)' },
-				to: { opacity: 1, transform: 'translateY(0px)' },
+			  from: { opacity: '0', transform: 'translateY(30px)' },
+			  to: { opacity: '1', transform: 'translateY(0)' },
 			},
 			pulse: {
-					'0%': { transform: 'scale(1)', boxShadow: '0 0 0 rgba(0,0,0,0.1)' },
-					'50%': { transform: 'scale(1.05)', boxShadow: '0 0 10px rgba(0,0,0,0.2)' },
-					'100%': { transform: 'scale(1)', boxShadow: '0 0 0 rgba(0,0,0,0.1)' },
+			  '0%': { transform: 'scale(1)', boxShadow: '0 0 0 rgba(0,0,0,0.1)' },
+			  '50%': { transform: 'scale(1.05)', boxShadow: '0 0 10px rgba(0,0,0,0.2)' },
+			  '100%': { transform: 'scale(1)', boxShadow: '0 0 0 rgba(0,0,0,0.1)' },
 			},
 		},
   	}
